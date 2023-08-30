724b0f0a65682f57874a39a759812d01d20aed48
Dan1ll2006 <143024022+Dan1ll2006@users.noreply.github.com>
2023-08-30 16:35:56 +0200

    Add files via upload

---
+++ JDK11Example/src/Main.java
@@ -0,0 +1,12 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+            String имя = "Данил";
+            int возрост = 17;
+            float вес = 80;
+
+            System.out.printf("имя: %s  возрост: %d  вес: %.2f \n", имя, возрост, вес);
+        }
+    }
